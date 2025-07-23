2dfac6d076aac7dfe3828d49096c3285917d68de
sunilswain7 <sunilkai320@gmail.com>
2025-07-23 21:59:58 +0530

    fix: edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For username.github.io root repo
+  base: './', // For username.github.io root repo
 })
